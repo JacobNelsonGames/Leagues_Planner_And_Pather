2ef4db8722bb85feec078071a0f77c9dc884f93d
Jacob Nelson <jacobnelsongames@gmail.com>
2023-09-30 14:49:47 -0500

    Adjusted difficulty colors

--- src/main/java/Posiedien_Leagues_Planner/TaskDifficulty.java
+++ src/main/java/Posiedien_Leagues_Planner/TaskDifficulty.java
@@ -18,13 +18,13 @@ public enum TaskDifficulty
         switch (InTaskDiff)
         {
             case BEGINNER:
-                return new Color(121, 67, 3,255);
+            return new Color(255, 142, 9,255);
             case EASY:
-                return new Color(255, 142, 9,255);
-            case MEDIUM:
                 return new Color(190, 190, 190,255);
+            case MEDIUM:
+                return new Color(25, 61, 13,255);
             case HARD:
-                return new Color(161, 250, 11,255);
+                return new Color(11, 250, 107,255);
             case ELITE:
                 return new Color(6, 193, 255,255);
             case MASTER:
